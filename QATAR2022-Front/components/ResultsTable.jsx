954d4d05dd621febc208ce3626453c51f04c136d
Juan Migone <juan.migone@cercle.ai>
2022-10-04 20:01:28 -0300

    Style adjustments

--- QATAR2022-Front/components/ResultsTable.jsx
+++ QATAR2022-Front/components/ResultsTable.jsx
@@ -2,18 +2,16 @@ import ResultsTableRow from './ResultsTableRow'
 
 function Group(props) {   
     return (
-            <div className="m-auto bg-transparent/[0.6] rounded-xl py-12 w-4/5" >
-                <br/>
-                <div className="text-white"> {props.title} </div>
-                <table id="" className="text-white w-full m-auto">
-                    <thead>
-                        <tr>
-                            <th>Pos</th>
-                            <th className="w-2/5">Wallet / Jugador</th>
-                            <th className="">Pts</th>
+            <div className="m-auto bg-transparent/[0.6] rounded-xl py-12 w-11/12 md:w-4/5 lg:w-3/5" >
+                <table id="" className="text-white w-full m-auto table-fixed">
+                    <thead className='w-1/2'>
+                        <tr className='w-full'>
+                            <th className="w-1/5">Pos</th>
+                            <th className="w-3/5">Wallet / Jugador</th>
+                            <th className="w-1/5">Pts</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody className='w-full'>
                         <ResultsTableRow/>
                         <ResultsTableRow/>
                         <ResultsTableRow/>
--- QATAR2022-Front/components/ResultsTableRow.jsx
+++ QATAR2022-Front/components/ResultsTableRow.jsx
@@ -1,12 +1,29 @@
 function GroupRow(props) {
+    // Captures 0x + 4 characters, then the last 4 characters.
+    const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
+    /**
+     * Truncates an ethereum address to the format 0x0000…0000
+     * @param address Full address to truncate
+     * @returns Truncated address
+     */
+    const truncateEthAddress = (address) => {
+    const match = address.match(truncateRegex);
+    if (!match) return address;
+    return `${match[1]}…${match[2]}`;
+    };
 
     return (
-        <tr className="h-10 w-4/5">
-            <td align="center" >0</td>
-            <td align="center" className="truncate w-4">0xdAC17F958D2ee523a2206206994597C13D831ec7</td>
-            <td align="center" >0</td>
+        <tr className="h-10 w-full">
+            <td align="center" className="w-1/5">0</td>
+            <td align="center" className="w-3/5 truncate">
+                <div className="w-full truncate">
+                    {truncateEthAddress("0xdAC17F958D2ee523a2206206994597C13D831ec7")}
+                </div>
+            </td>
+            <td align="center" className="w-1/5">0</td>
         </tr>
     );
   }
 
 export default GroupRow;
+
