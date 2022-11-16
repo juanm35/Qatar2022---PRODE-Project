e52bf1ba18adeed92c3809d57ac2a5dbe729ff59
juanm35 <jjmigone@gmail.com>
2022-11-16 17:18:05 -0300

    Adding player aliases logic

--- QATAR2022-Front/components/ResultsTableRow.jsx
+++ QATAR2022-Front/components/ResultsTableRow.jsx
@@ -13,12 +13,20 @@ function GroupRow(props) {
     return `${match[1]}…${match[2]}`;
     };
 
+    const nickNames = [
+        {address: '0xb6e2Ab44Ca614E654905C40372518343788238EA', alias: "Juankman Friedone"},
+        {address: '0x1446103c5994c8a1c0dc55ab353bacec3c88a4c6', alias: "Tomson"},
+        {address: '0x4e103a7aF4D148793304e317728bA0d5D5ea2a3C', alias: "El Corren"},
+    ]
+
+    const currentAlias =  nickNames.find((element) => element.address === props.address)?.alias ??  truncateEthAddress(props.address)
+
     return (
         <tr className={`${props.currentPlayer? 'bg-qatarSilver text-qatarRed' :''} h-10 w-full`}>
             <td align="center" className="w-1/5">{props.index+1}</td>
             <td align="center" className="w-3/5 truncate">
                 <div className="w-full truncate">
-                    {props.address === '0xb6e2Ab44Ca614E654905C40372518343788238EA'? "Juankman Friedone" : truncateEthAddress(props.address)}
+                    {currentAlias}
                 </div>
             </td>
             <td align="center" className="w-1/5">{props.score}</td>
